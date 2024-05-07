bd20968c2d03b7caac9ee710ef68ea5d1180d9a8
Brittany Campos <bmagnu1@wgu.edu>
2024-05-06 19:29:36 -0500

    .

--- sterilecalc.js
+++ sterilecalc.js
@@ -1,9 +1,4 @@
-function convertdose() {
-    const userDoseNum = parseFloat(document.getElementById('convert_doseNum').value);
-    const doseUnit = document.getElementById('convert_doseUnit').value;
-    const userStrengthNum = parseInt(document.getElementById('convert_strengthNum').value);
-    const strengthUnit = document.getElementById('convert_strengthUnit').value;
-    const userStrengthVolNum = parseFloat(document.getElementById('convert_strengthVolNum').value);
+function convertdose(userDoseNum, doseUnit, userStrengthNum, strengthUnit, userStrengthVolNum) {
     let calcDoseNum, calcStrengthNum;
 
     // Check if userDoseNum or userStrengthNum are NaN
@@ -40,6 +35,16 @@ function convertdose() {
 
     let result = (calcDoseNum / calcStrengthNum) * userStrengthVolNum;
     let resultunits = result * 100;
-    document.getElementById("convert_result").innerHTML = "If the patient requires a dose of " + userDoseNum + doseUnit + ", and the drug product contains " + userStrengthNum + strengthUnit + " per every " + userStrengthVolNum + "mL, they would need to inject " + result + " mL or " + resultunits + " units.";
+    return [result, resultunits];
 }
 
+function printInjectionVol(element) {
+    const userDoseNum = parseFloat(document.getElementById('convert_doseNum').value);
+    const doseUnit = document.getElementById('convert_doseUnit').value;
+    const userStrengthNum = parseInt(document.getElementById('convert_strengthNum').value);
+    const strengthUnit = document.getElementById('convert_strengthUnit').value;
+    const userStrengthVolNum = parseFloat(document.getElementById('convert_strengthVolNum').value);
+    const result = convertdose(userDoseNum, doseUnit, userStrengthNum, strengthUnit, userStrengthVolNum);
+    document.getElementById(element).innerHTML = "If the patient requires a dose of " + userDoseNum + doseUnit + ", and the drug product contains " + userStrengthNum + strengthUnit + " per every " + userStrengthVolNum + "mL, they would need to inject " + result[0] + " mL or " + result[1] + " units.";
+
+}
