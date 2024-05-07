07c1a2955803c7954a3762afb6fbd8e34fa30793
Brittany Campos <bmagnu1@wgu.edu>
2024-05-06 19:29:32 -0500

    split convert function into math and print function

---
+++ sterilecalc.js
@@ -0,0 +1,45 @@
+function convertdose() {
+    const userDoseNum = parseFloat(document.getElementById('convert_doseNum').value);
+    const doseUnit = document.getElementById('convert_doseUnit').value;
+    const userStrengthNum = parseInt(document.getElementById('convert_strengthNum').value);
+    const strengthUnit = document.getElementById('convert_strengthUnit').value;
+    const userStrengthVolNum = parseFloat(document.getElementById('convert_strengthVolNum').value);
+    let calcDoseNum, calcStrengthNum;
+
+    // Check if userDoseNum or userStrengthNum are NaN
+    console.log(`userDoseNum is ${userDoseNum} => ${isNaN(userDoseNum)}`)
+    console.log(`userStrengthNum is ${userStrengthNum} => ${isNaN(userStrengthNum)}`)
+    if (isNaN(userDoseNum) || isNaN(userStrengthNum)) {
+        alert("You are missing a value; please complete all fields and try again.");
+        document.getElementById("convert_result").innerHTML = "Error";
+        return; // Exit the function early if there's an error
+    }
+
+    // Convert Units all units to mg
+    if (doseUnit === 'mg') {
+        calcDoseNum = userDoseNum
+    }
+    if (strengthUnit === 'mg') {
+        calcStrengthNum = userStrengthNum
+    }
+    if (doseUnit === 'mcg') {
+        calcDoseNum = userDoseNum / 1000
+    }
+    if (doseUnit === 'g') {
+        calcDoseNum = userDoseNum * 1000
+    }
+    if (strengthUnit === 'mg') {
+        calcStrengthNum = userStrengthNum
+    }
+    if (strengthUnit === 'mcg') {
+        calcStrengthNum = userStrengthNum / 1000
+    }
+    if (strengthUnit === 'g') {
+        calcStrengthNum = userStrengthNum * 1000
+    }
+
+    let result = (calcDoseNum / calcStrengthNum) * userStrengthVolNum;
+    let resultunits = result * 100;
+    document.getElementById("convert_result").innerHTML = "If the patient requires a dose of " + userDoseNum + doseUnit + ", and the drug product contains " + userStrengthNum + strengthUnit + " per every " + userStrengthVolNum + "mL, they would need to inject " + result + " mL or " + resultunits + " units.";
+}
+
